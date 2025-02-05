f9cef7a29c2962bc0962adda9aef8b369f492bc7
SinghGursher <gurshers101@gmail.com>
2025-02-05 14:44:09 +0300

    Final

---
+++ java.js
@@ -0,0 +1,9 @@
+// Function to display Date and Time
+function displayDate() {
+    document.getElementById('demo').innerHTML = Date();
+}
+
+// Function to change text content
+function changeText() {
+    document.getElementById("demo1").innerHTML = "Bye World.";
+}
